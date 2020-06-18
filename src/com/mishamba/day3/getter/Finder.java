38b2e547201827ec55683d13d0b9ebc1eb0af902
Mishamba <misha.nenahov@gmail.com>
2020-06-18 12:02:19 +0300

    session test works
    rewrote sessionservice
    added finder

---
+++ src/com/mishamba/day3/getter/Finder.java
@@ -0,0 +1,46 @@
+package com.mishamba.day3.getter;
+
+import com.mishamba.day3.entity.ball.Ball;
+import com.mishamba.day3.entity.basket.Basket;
+import com.mishamba.day3.exception.ProgramException;
+import com.mishamba.day3.parser.Parser;
+import com.mishamba.day3.reader.BallBasketReader;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Finder {
+    public List<Ball> findBalls() {
+        BallBasketReader ballBasketReader = new BallBasketReader();
+        Parser parser = new Parser();
+
+        Path ballPath = Paths.get(
+                "/home/mishamba/java/EPAM_JWD_task3/data/ballData");
+        List<String> ballCharacteristics = ballBasketReader.getLines(ballPath,
+                "for balls");
+        ArrayList<Ball> balls = new ArrayList<>();
+        for (String line : ballCharacteristics) {
+            balls.add(parser.parseStringToBall(line));
+        }
+
+        return balls;
+    }
+
+    public List<Basket> findBaskets() throws ProgramException {
+        BallBasketReader ballBasketReader = new BallBasketReader();
+        Parser parser = new Parser();
+
+        Path basketPath = Paths.get(
+                "/home/mishamba/java/EPAM_JWD_task3/data/basketData");
+        List<String> basketCharacteristics = ballBasketReader.
+                getLines(basketPath, "for basket");
+        ArrayList<Basket> baskets = new ArrayList<>();
+        for (String line : basketCharacteristics) {
+            baskets.add(parser.parseStringToBasket(line));
+        }
+
+        return baskets;
+    }
+}
--- src/com/mishamba/day3/service/session/SessionService.java
+++ src/com/mishamba/day3/service/session/SessionService.java
@@ -4,37 +4,18 @@ import com.mishamba.day3.entity.ball.Ball;
 import com.mishamba.day3.entity.ball.configuration.BallColor;
 import com.mishamba.day3.entity.basket.Basket;
 import com.mishamba.day3.exception.ProgramException;
-import com.mishamba.day3.parser.Parser;
-import com.mishamba.day3.reader.BallBasketReader;
+import com.mishamba.day3.getter.Finder;
 import com.mishamba.day3.service.process.PutService;
 import com.mishamba.day3.service.process.ResultService;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 public class SessionService {
     public void start() throws ProgramException {
-        BallBasketReader ballBasketReader = new BallBasketReader();
-        Parser parser = new Parser();
+        Finder finder = new Finder();
 
-        Path ballPath = Paths.get(
-                "/home/mishamba/java/EPAM_JWD_task3/data/ballData");
-        List<String> ballCharacteristics = ballBasketReader.getLines(ballPath, "for balls");
-        ArrayList<Ball> balls = new ArrayList<>();
-        for (String line : ballCharacteristics) {
-            balls.add(parser.parseStringToBall(line));
-        }
-
-        Path basketPath = Paths.get(
-                "/home/mishamba/java/EPAM_JWD_task3/data/basketData");
-        List<String> basketCharacteristics = ballBasketReader.
-                getLines(basketPath, "for basket");
-        ArrayList<Basket> baskets = new ArrayList<>();
-        for (String line : basketCharacteristics) {
-            baskets.add(parser.parseStringToBasket(line));
-        }
+        List<Ball> balls = finder.findBalls();
+        List<Basket> baskets = finder.findBaskets();
 
         PutService putService = new PutService();
         boolean putState = true;
